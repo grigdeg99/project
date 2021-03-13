8bfc6814d9d4b82596243310d3668b7d675f7359
Alex <example@gmail.com>
2021-03-13 05:13:21 +0300

    13.03.2021 First project

---
+++ js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// console.log(5 / 0);
+// console.log('saa' * 3);
+// let lol;
+// console.log(lol);
+
+// let person = {
+//    name: "John",
+//    age: 22,
+//    isMarried: false
+// };
+
+// console.log(person.isMarried);
+
+// let arr = ["John", "Elice", "Bill"];
+// console.log(arr[0]);
+
+//alert("Hello world!");
+
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли Вам 18 ?", "Да");
+
+// console.log(typeof (answer));
+
+// console.log();
+
+let incr = 10,
+   decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
