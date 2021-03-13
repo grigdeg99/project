053622cb7d813f5e9618a01b73742220b8d32ce0
Alex <example@gmail.com>
2021-03-13 06:24:57 +0300

    new projects

--- js/script.js
+++ js/script.js
@@ -1,38 +1,25 @@
 'use strict';
 
-// console.log(5 / 0);
-// console.log('saa' * 3);
-// let lol;
-// console.log(lol);
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let expensesSale = prompt("Введите обязательную статью расходов в этом месяце", "");
+let expensesMoney = prompt("Во сколько обойдется", "");
+let oneDayMoney;
 
-// let person = {
-//    name: "John",
-//    age: 22,
-//    isMarried: false
-// };
+let appData = {
+   cash: money,
+   timeData: time,
+   expenses: {
+      expensesSale: expensesMoney
+   },
+   optionalExpenses: {
 
-// console.log(person.isMarried);
-
-// let arr = ["John", "Elice", "Bill"];
-// console.log(arr[0]);
-
-//alert("Hello world!");
+   },
+   income: [],
+   savings: false
 
 
-// let answer = confirm("Are you here?");
 
-// console.log(answer);
 
-// let answer = +prompt("Есть ли Вам 18 ?", "Да");
-
-// console.log(typeof (answer));
-
-// console.log();
-
-let incr = 10,
-   decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5 % 2);
+};
+alert(oneDayMoney);
